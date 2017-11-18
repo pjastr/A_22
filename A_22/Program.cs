cdeebc088bdd6e99d894c6598e0562877df35023
Piotrek <@>
2017-11-18 14:36:42 +0100

    przeslanie plików

---
+++ A_22/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A_22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 10; j >= i; j--)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 1; j < 2 * i; j++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+
+                for (int j = -2; j <= i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 14; j >=2*i; j--)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
